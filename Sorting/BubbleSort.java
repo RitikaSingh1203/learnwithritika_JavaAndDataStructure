2aa79a2254a6009b99186ad90a42ae672aa0ca47
RitikaSingh1203 <singh26.juhi@gmail.com>
2026-05-09 16:12:21 +0530

    sorting

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+public class BubbleSort {
+    public static void Bubble_Sort(int arr[]){
+        for(int turn = 0; turn<arr.length; turn++){
+            for(int j = 0;j<arr.length-1-turn;j++){
+                if(arr[j]>arr[j+1]){
+                  //swap
+                  int temp = arr[j];
+                  arr[j] = arr[j+1];
+                  arr[j+1] = temp;  
+                }
+            }
+
+        }
+
+    }
+    public static void Print_BubbleSort(int arr[]){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+        Bubble_Sort(arr);
+        Print_BubbleSort(arr);
+    }
+}
