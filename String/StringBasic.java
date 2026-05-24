a53af624d62af4176933c10feb643c4ddfeafef5
RitikaSingh1203 <singh26.juhi@gmail.com>
2026-05-24 23:39:25 +0530

    StringBasic

---
+++ String/StringBasic.java
@@ -0,0 +1,16 @@
+package String;
+
+public class StringBasic {
+    
+
+    // Main Function
+    public static void main(String args[])
+    {
+
+        // creating Java string using a new keyword
+        String str = new String("Geeks");
+
+        System.out.println(str);
+    }
+}
+
